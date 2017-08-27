5e9cea467d1ed9adf5d7d19042e0c8bbdfa2bc1c
florianmarsch <florianmarsch@192.168.0.143>
2017-08-27 11:40:27 +0200

    parse body after login

--- passport/login.js
+++ passport/login.js
@@ -39,7 +39,7 @@ module.exports = function(passport){
 	                            return done(null, false, req.flash('message', 'Profile Not found.'));   
 	        			    }else{
 	        			    		console.log("login : "+body);
-	        			    		return done(null, body);
+	        			    		return done(null, JSON.parse(body));
 	        			    }
 	        			  });
                     
